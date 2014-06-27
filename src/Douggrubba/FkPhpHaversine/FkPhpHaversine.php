deb81c5f2f2d67db9ef8dbe3f1be5c685043f611
Doug Grubba <douglas.grubba@gmail.com>
2014-06-26 23:25:44 -0400

    port over some of the old code

--- src/Douggrubba/FkPhpHaversine/FkPhpHaversine.php
+++ src/Douggrubba/FkPhpHaversine/FkPhpHaversine.php
@@ -45,6 +45,45 @@ class FkPhpHaversine implements FkPhpHaversineInterface
     public function closest($lat, $lng, $max_distance = 25, $max_locations = 10, $units = 'miles', $fields = false)
     {
 
+        // Allow for changing of units of measurement
+        switch ($units) {
+            case 'miles':
+                //radius of the great circle in miles
+                $gr_circle_radius = 3959;
+                break;
+            case 'kilometers':
+                //radius of the great circle in kilometers
+                $gr_circle_radius = 6371;
+                break;
+        }
+
+        // Support the selection of certain fields
+        if ( ! $fields) {
+            $fields = ['*'];
+        }
+
+
+        // Generate the select field for disctance
+        $disctance_select = sprintf(
+            "( %d * acos( cos( radians(%s) ) " .
+            " * cos( radians( lat ) ) " .
+            " * cos( radians( lng ) - radians(%s) ) " .
+            " + sin( radians(%s) ) * sin( radians( lat ) ) " .
+            ") " .
+            ") " .
+            "AS distance",
+            $gr_circle_radius,
+            $lat,
+            $lng,
+            $lat
+        );
+
+        return DB::table($table_name)
+            ->having('distance', '<', $max_distance)
+            ->take($max_locations)
+            ->order_by('distance', 'ASC')
+            ->get([$fields, $disctance_select]);
+
     }
 
 }
